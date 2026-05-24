44654e714e50b018a8e811c5c43c7c68d9710a6a
Somdatta_Sikdar <somdatta2312@gmail.com>
2026-05-24 07:51:34 +0530

    New Empty Project Created

---
+++ DynamicFormBuilder/DynamicFormBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicFormBuilderApp.swift
+//  DynamicFormBuilder
+//
+//  Created by Somdatta Sikdar on 21/05/26.
+//
+
+import SwiftUI
+
+@main
+struct DynamicFormBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
